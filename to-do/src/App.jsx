2b01bd801998ecb21bc8052c0f8efce3ab68240d
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-30 14:53:29 +0530

    create-handleAdd-and-handleChange-functions

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -3,7 +3,19 @@ import Navbar from './component/Navbar';
 
 
 function App() {
+  const[task, setTask] = useState("");
+  const[addedTaks, setAddedTasks] = useState([]);
+  
+  //To add a new task.
+  const handleAdd = () => {
+    setAddedTasks([...addedTaks, {task, isCompleted: false}]);
+    setTask("")
+  }
 
+  //To empty input field after the task is added.
+  const handleChange = (work) => {
+    setTask(work.target.value);
+  }
 
   return (
     <>
@@ -12,19 +24,18 @@ function App() {
         <div className=''>
           <div className="addToDo">
             <h2 className='text-xl font-semibold my-1'>Add a Task</h2>
-            <input type="text" className='w-1/3'/>
-            <button className='bg-pink hover:bg-hoverpink p-3 py-1 text-white text-sm rounded-md mx-7'>Add</button>
+            <input onChange={handleChange} value={task} type="text" className='w-1/3'/>
+            <button onClick={handleAdd} className='bg-pink hover:bg-hoverpink p-3 py-1 text-white text-sm rounded-md mx-7'>Add</button>
           </div>
           <h2 className='text-xl font-semibold my-5'>Your Tasks</h2>
 
           <div className="todos">
             <div className="todo flex">
-              <div className="text">
+              <div className="text">My Tasks </div>
                 <div className="buttons">
                   <button className='bg-pink hover:bg-hoverpink p-3 py-1 text-white text-sm rounded-md mx-2'>Edit</button>
                   <button className='bg-pink hover:bg-hoverpink p-3 py-1 text-white text-sm rounded-md mx-2'>Delete</button>
                 </div>
-              </div>
             </div>
           </div>
         </div>
