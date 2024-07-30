695cd465434675b2cde6ecb2a1644beff69935eb
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-30 14:18:51 +0530

    nav-bar

--- to-do/tailwind.config.js
+++ to-do/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+
   theme: {
+    colors: {
+      pink: '#A87676',
+      white: '#ffffff',
+    },
     extend: {},
   },
   plugins: [],
